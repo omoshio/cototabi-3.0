21aba268bcd24012df218b673f9d69a81692c9f0
Osamu Moshio <osamu.moshio@gmail.com>
2025-09-15 08:35:49 +0900

    Vueコンポーネントを分割して使用可能に変更

--- front-page.php
+++ front-page.php
@@ -11,7 +11,10 @@
   <div class="l-main">
     <section>
     <h1>WordPress × Vue テストページ Today</h1>
-    <div id="App"></div>
+    <!--<div id="App"></div>　Vueコンポーネントをまとめて使う場合　-->
+	<div id="Hello"></div>
+	<div id="Slider"></div>
+	<div id="Tabs"></div>
   </section>
     
     <section class="p-fv">
--- frontend/main.js
+++ frontend/main.js
@@ -1,4 +1,28 @@
+/* Vueコンポーネントをまとめて使う場合
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#App')
+createApp(App).mount('#App')*/
+
+import { createApp } from 'vue'
+import Hello from './components/Hello.vue'
+import Slider from './components/Slider.vue'
+import Tabs from './components/Tabs.vue'
+
+// Hello
+const helloEl = document.querySelector('#Hello')
+if (helloEl) {
+  createApp(Hello).mount(helloEl)
+}
+
+// Slider
+const sliderEl = document.querySelector('#Slider')
+if (sliderEl) {
+  createApp(Slider).mount(sliderEl)
+}
+
+// Tabs
+const tabsEl = document.querySelector('#Tabs')
+if (tabsEl) {
+  createApp(Tabs).mount(tabsEl)
+}
